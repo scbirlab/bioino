31b713f401e760a1d6257af25a89909e4a541fc6
Eachan Johnson <eachan.johnson@crick.ac.uk>
2026-05-09 17:07:57 +0100

    Fix imports

--- bioino/__init__.py
+++ bioino/__init__.py
@@ -4,4 +4,6 @@ appname = "bioino"
 __version__ = version(appname)
 
 from .fasta import *
-from .gff import *
+from .gff.column import GffColumns
+from .gff.gff import GffFile
+from .gff.line import GffLine
